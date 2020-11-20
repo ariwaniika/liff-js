e41cde9fbce91cc48fd7706f2d460af865a45bba
ariwaniika <70216392+ariwaniika@users.noreply.github.com>
2020-11-21 01:08:08 +0900

    Update liff.js

--- liff.js
+++ liff.js
@@ -1,16 +1,19 @@
+document.addEventListener("DOMContentLoaded", () => {
 liff.init({
 // 自分のLIFF ID（URLから『https://liff.line.me/』を除いた文字列）を入力する
 liffId: "1654893906-zZ9xVQ4W"
 
 }).then(() => { // 初期化完了. 以降はLIFF SDKの各種メソッドを利用できる
 
+    
+    sendText("send message");
 // 利用者のLINEアカウントのプロフィール名を取得
 liff.getProfile().then(profile => {
 
 
 })
 })
-
+}
 
 
 
