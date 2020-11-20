26a277a680cafc1c29e3bbd06bf691e09809a015
ariwaniika <70216392+ariwaniika@users.noreply.github.com>
2020-11-20 23:40:44 +0900

    Update liff.js

--- liff.js
+++ liff.js
@@ -1,32 +1,4 @@
-<!--
 
-$(document).ready(function () {
-    // liffId: LIFF URL "https://liff.line.me/xxx"のxxxに該当する箇所
-    // LINE DevelopersのLIFF画面より確認可能
-    var liffId = "1654893906-zZ9xVQ4W";
-    initializeLiff(liffId);
-    sendText("send massage!!");
-    shareTargetPicker("send massage!!!");
-})
-
-function initializeLiff(liffId) {
-    liff
-        .init({
-            liffId: liffId
-        })
-        .then(() => {
-            // Webブラウザからアクセスされた場合は、LINEにログインする
-            if (!liff.isInClient() && !liff.isLoggedIn()) {
-                window.alert("LINEアカウントにログインしてください。");
-                liff.login({redirectUri: location.href});
-            }
-        })
-        .catch((err) => {
-            console.log('LIFF Initialization failed ', err);
-        });
-}
-
--->
 
 function sendText(text) {
     if (!liff.isInClient()) {
